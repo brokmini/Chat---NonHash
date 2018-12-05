89758ec6457fcc124cb3c9c15bbd449cbb61557e
Abirukmani Venkatachalam <35310664+brokmini@users.noreply.github.com>
2018-12-05 12:36:23 -0600

    Including Ping Pong as of now

--- Peer.java
+++ Peer.java
@@ -7,9 +7,15 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.*;
 import java.util.Random;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Peer
 {
+  static Hashtable<String,Socket> hashtable = new Hashtable<String,Socket>();
+  static String name;
   public static void main(String[] args) throws IOException
   {
         File file = new File("peers.csv");
@@ -23,7 +29,7 @@ public class Peer
         {
           //If it is the first node to join the p2p network
           System.out.println("Hey! Choose username");
-          String name=scanner.nextLine();
+          name=scanner.nextLine();
           pw.write(name+",9000");
           pw.close();
           Integer listen_port = 9000;
@@ -34,7 +40,7 @@ public class Peer
         {
           //When peer is not the first to join the network
           Boolean nameSet = false;
-          String userList="",name="";
+          String userList="";
           String portNumbers="";
           while (!nameSet)
           {
@@ -95,6 +101,8 @@ public class Peer
           }
         }
 
+  PingSender ps = new PingSender();
+  ps.start();
 
   } //End of Main function
 
@@ -106,11 +114,16 @@ public class Peer
 				{
 				Socket socket = new Socket("localhost", port);
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-				out.write(String.valueOf(myPort)+" Connect \n");
+				out.write("CONNECTED WITH "+Peer.name+"\n");
 				out.flush();
-				//System.out.println("I am trying to connect: "+socket);
 
-				MessageListener p = new MessageListener(socket);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String recdData = in.readLine();
+        System.out.println(recdData);
+				String username = recdData.split("CONNECTED WITH")[1];
+        Peer.myHash(username,socket);
+
+				PingListener p = new PingListener(socket,myPort,username);
 				p.start();
 				}
 				catch (IOException e)
@@ -118,8 +131,83 @@ public class Peer
 					System.out.println("Connect Failed"); System.exit(-1);
 				}
 			}
-} // End of class Peer
 
+//**********************************************************************************************************************************************
+//This function handles the sending of Ping messages to all neighbors
+public static int pingStatus()
+{
+          String connectedUsers = Peer.readHash();
+          System.out.println("The connected users are - "+connectedUsers);
+          String[] friendList = connectedUsers.split(",");
+          for(int i =0;i<friendList.length;i++)
+          {
+            //System.out.println(fetchSocket(friendList[i]));
+            Socket t = fetchSocket(friendList[i]);
+            try
+            {
+                PrintWriter pOut = new PrintWriter(t.getOutputStream(), true);
+                pOut.write("Hello from "+Peer.name+"\n");
+                pOut.flush();
+
+            }
+            catch (NullPointerException e)
+            {
+              System.out.println("PING STATUS - "+friendList[i] +" has vanished");
+              Peer.hashtable.remove(friendList[i]);
+              //myList=Agent.readHash();
+            }
+            catch (IOException e) {}
+          }
+          return 1;
+}
+//**********************************************************************************************************************************************
+//function to hash neighbor details
+public static void myHash(String username,Socket s)
+{
+      				hashtable.put(username,s);
+}
+//**********************************************************************************************************************************************
+//function to read neighbor details from hash
+public static String readHash()
+  {
+              Set<String> keys = hashtable.keySet();
+              Iterator<String> itr = keys.iterator();
+              String availableUsers =""; int first=1;
+              while (itr.hasNext())
+              {
+                // Getting Key
+                String key = itr.next();
+                if(first==1)
+                {
+                  availableUsers = availableUsers + key;
+                  first = 0;
+                }
+                else
+                {
+                  availableUsers = availableUsers + ","+key;
+                }
+              }
+      				return availableUsers;
+  }
+//**********************************************************************************************************************************************
+//function to read neighbor details from hash
+ public static Socket fetchSocket(String username)
+ {
+              Socket s = null;
+              Set<String> keys = hashtable.keySet();
+              Iterator<String> itr = keys.iterator();
+              while (itr.hasNext())
+              {
+                // Getting Key
+                String key = itr.next();
+                if(key.equals(username))
+                {
+                  s= hashtable.get(key);
+                }
+              }
+        return s;
+  }
+} // End of class Peer
 //**********************************************************************************************************************************************
 //This thread actively listens for any nodes connecting to it
 class Listener extends Thread
@@ -146,14 +234,15 @@ class Listener extends Thread
 						{
 									try
 									{
-										System.out.println("Listening...");
+										System.out.println("Looking out for new friends...");
 										clientSocket = serverSocket.accept();
-										MessageListener l=new MessageListener(clientSocket);
+
+                    PongListener l=new PongListener(clientSocket);
 										l.start();
 
                     //To advertise about self
 										PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-										out.write(String.valueOf(port)+" Init\n");
+										out.write("CONNECTED WITH "+Peer.name+"\n");
 										out.flush();
 
 
@@ -167,12 +256,98 @@ class Listener extends Thread
 
 } //End of Listener class
 //***********************************************************************************************************************************************
+class PingSender extends Thread
+{
+			public void run()
+			{
+				while(true)
+				{
+						try
+						{
+							Thread.sleep(15000);
+							int pingDone=Peer.pingStatus();
+						}
+						catch(InterruptedException e)
+		        {
+		               System.err.println("IOException " + e);
+		        }
+				}
+			}
+}
+//**********************************************************************************************************************************************
+class PingListener extends Thread
+{
+				Socket socket;
+				Integer hostPort;
+        String friend;
+				//Integer backupHost,my_no_files;
 
-class MessageListener extends Thread
+				public PingListener(Socket s,Integer l,String friend)
+				{
+					socket = s;
+					hostPort=l;
+          friend = friend;
+				}
+
+				public void run()
+				{
+								while(true)
+								{
+									try
+									{
+										BufferedReader pIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+										String ping=pIn.readLine();
+										if (ping!= null && !ping.isEmpty())
+
+												{
+															//String myList= Agent.readHash();
+															String[] pingMsg=ping.split("#");
+															/*if (pingMsg.length>1)
+															{
+																//Identifies node to connect to incase of loss of neighbor
+																backupHost=Integer.parseInt(pingMsg[1]);
+															}*/
+															if(pingMsg[0].contains("Hello from"))
+															{
+                                System.out.println(pingMsg[0]);
+																PrintWriter pong = new PrintWriter(socket.getOutputStream(), true);
+																pong.write("HelloAck from "+Peer.name+"\n");
+																pong.flush();
+															}
+
+
+												}
+									}//End of Try
+									catch (Exception e)
+									{
+										//If the node to which I initially connected is dead, I maintain my presence in the sytem by reconnecting to another host
+										System.out.println("Neighbor I initially connected with has vanished");
+										//Peer.hashtable.remove(friend);
+										/*Agent.myList=Agent.readHash();
+										Reconnector r = new Reconnector(backupHost,Agent.listen_port,my_no_files);
+										r.start();
+										try
+										{
+											Thread.sleep(Long.MAX_VALUE);
+										}
+										catch(InterruptedException q)
+										{
+											System.out.println("Cdnt Sleep "+q);
+
+										}*/
+
+									}
+
+								}
+				}
+}
+//******************************************************************************************************************************************************************
+class PongListener extends Thread
 {
 
 			Socket socket;
-			public MessageListener(Socket s)
+      String friend;
+			public PongListener(Socket s)
 				{
 				socket=s;
 				}
@@ -184,13 +359,36 @@ class MessageListener extends Thread
 					{
 						BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 						String recdData=in.readLine();
-            System.out.println("MESSAGE RECEIVED IS : \n");
-            System.out.println(recdData);
-          } //End of Try
-          catch(IOException e)
-  				{
-          }
-        } //End of while
+            //System.out.println("PONG LISTEN: "+recdData);
+						if(recdData.contains("CONNECTED WITH "))
+						{
+							//Fetches details about a new node that connects with it and hashes the information
+      				friend = recdData.split("CONNECTED WITH ")[1];
+              Peer.myHash(friend,socket);
+						}
 
-      } // End of run
-} // End of MessageListener Class
+						else if(recdData.contains("HelloAck"))
+						{
+							System.out.println(recdData);
+						}
+					} // End of Try
+					catch(IOException e)
+					{
+							//In case a node that connected to it is dead
+							System.out.println("PongListener - "+friend +" has vanished");
+							Peer.hashtable.remove(friend);
+							//Agent.myList=Agent.readHash();
+							try
+							{
+								Thread.sleep(Long.MAX_VALUE);
+							}
+							catch(InterruptedException q)
+							{
+								System.out.println("Cdnt Sleep "+q);
+
+							}
+					}
+				}
+			}
+}
+//****************************************************************************************************************************************************
